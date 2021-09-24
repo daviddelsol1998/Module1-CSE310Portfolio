05d31b2dcec4e70cceaae2c1c1fe0599a502ea58
David <daviddelsol1998@gmail.com>
2021-09-24 07:53:06 -0600

    learned to work with Data Structures

--- project_files/learning_exercises/data_structures/main.py
+++ project_files/learning_exercises/data_structures/main.py
@@ -1,1 +1,24 @@
 '''This file will be used to explore data structures within Python'''
+import pandas as pd
+from datetime import date
+
+# get user input 3 times
+amounts = {'Date':[],'Amount':[]}
+# #create df
+# df = pd.DataFrame.from_dict(amounts)
+# df.to_csv('amounts_bydate.csv')
+
+
+for i in range(3):
+    user_input = input('Please enter amount: ')
+    now_date = date.today()
+    amounts['Date'].append(now_date)
+    amounts['Amount'].append(user_input)
+
+print(amounts)
+
+#create df
+df = pd.DataFrame.from_dict(amounts)
+df.to_csv('amounts_bydate.csv', mode='a', index=False, header= False)
+
+
